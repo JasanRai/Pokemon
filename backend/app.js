f494ad74f54d4e9f6ed93a6cee8135ba7468df2d
Jasan <jasanrai95@hotmail.com>
2022-02-04 16:12:36 +1100

    pulling required data such as img and types for our array

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+
+const app = express();
+const port = 5001;
+
+app.get("/", (req, res) => {
+  axios
+    .get("https://pokeapi.co/api/v2/pokemon?limit=10")
+    .then(async (response) => {
+      const allPokemon = [];
+      const pokeData = response.data.results;
+      for (let i = 0; i < pokeData.length; i++) {
+        let updatedPokeData = {};
+        updatedPokeData["name"] = pokeData[i].name;
+
+        const response = await axios.get(pokeData[i].url);
+        updatedPokeData["types"] = response.data.types;
+        updatedPokeData["img"] =
+          response.data.sprites.other.dream_world.front_default;
+        allPokemon.push(updatedPokeData);
+      }
+      res.send(allPokemon);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`App Listening on http://localhost:${port}`);
+});
